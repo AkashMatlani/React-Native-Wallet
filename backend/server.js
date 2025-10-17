c864798979f029b5a893289632f20ba73f901d68
Akash Matlani <aakash.matlani@yahoo.com>
2025-10-17 15:15:56 +0100

    Database Initalized

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,37 @@
 import express from "express";
+import dotenv from "dotenv";
+import { sql } from "./config/db.js";
 
+dotenv.config();
 const app = express();
 
-app.get("/",(req,res) =>{
-    res.send("It's Working with update perfect");
+const PORT = process.env.PORT || 5001;
+
+async function initDB() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transcations(
+        id SERIAL PRIMARY KEY,
+        user_id VARCHAR(255) NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        amount DECIMAL(10,2) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`;
+
+    console.log("Database initalized successfully");
+  } catch (error) {
+    console.log("error initalizing database", error);
+    process.exit(1); //status code 1 faliure , 0 Success
+  }
+}
+app.get("/", (req, res) => {
+  res.send("It's Working with update");
 });
 
-app.listen(5001, () => {
-  console.log("Server is up and running on PORT:5001");
+console.log("my port", process.env.PORT);
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is up and running on PORT:", PORT);
+  });
 });
