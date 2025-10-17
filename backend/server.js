722739e028491b20fd67ec99afc3724ba95f80ae
Akash Matlani <aakash.matlani@yahoo.com>
2025-10-17 13:55:51 +0100

    Nodemon added

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
 import express from "express";
+
+const app = express();
+
+app.get("/",(req,res) =>{
+    res.send("It's Working with update perfect");
+});
+
+app.listen(5001, () => {
+  console.log("Server is up and running on PORT:5001");
+});
