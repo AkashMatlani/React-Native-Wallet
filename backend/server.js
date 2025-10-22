1cef67a90b9484c993194152d14893479757bc12
Akash Matlani <aakash.matlani@yahoo.com>
2025-10-22 12:40:19 +0100

    Code Optimization For Route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import { sql } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+import transactionRoute from "./routes/transactionRoute.js"
 
 dotenv.config();
 const app = express();
@@ -11,6 +11,7 @@ const app = express();
 app.use(rateLimiter)
 
 app.use(express.json());
+
 
 const PORT = process.env.PORT || 5001;
 
@@ -32,94 +33,8 @@ async function initDB() {
   }
 }
 
-app.post("/api/transcations", async (req, res) => {
-  //title, amount, category, user_id
-  try {
-    const { title, amount, category, user_id } = req.body;
-    if (!title || !category || !user_id || amount === undefined) {
-      return res.status(400).json({ message: "All Fields are required" });
-    }
-
-    const transaction =
-      await sql` INSERT INTO transcations(user_id,title,amount,category)
-      VALUES (${user_id},${title},${amount},${category}) 
-      RETURNING *
-      `;
-    console.log(transaction);
-    res.status(201).json(transaction[0]);
-  } catch (error) {
-    console.log("Error Creating the transaction", error);
-    res.status(500).json({ message: "Internel Server Error" });
-  }
-});
-
-//Get Request
-app.get("/api/transcations/:user_Id", async (req, res) => {
-  try {
-    const { user_Id } = req.params;
-    console.log(user_Id);
-
-    const transactions =
-      await sql` Select * From transcations where user_id= ${user_Id} 
-     ORDER BY created_at DESC `;
-
-    res.status(200).json(transactions);
-  } catch (error) {
-    console.log("Error getting the transaction", error);
-    res.status(500).json({ message: "Internel Server Error" });
-  }
-});
-
-//Delete  Request
-app.delete("/api/transcations/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    if (isNaN(parseInt(id))) {
-      return res.status(400).json({ message: "Invalid transaction ID" });
-    }
-    const transactions =
-      await sql` Delete From transcations where id= ${id}  RETURNING *`;
-    if (transactions.length === 0) {
-      return res.status(400).json({ message: "Transcations not found" });
-    }
-    res.status(200).json({ message: "Transcation deleted Successfully" });
-  } catch (error) {
-    console.log("Error deletig the transaction", error);
-    res.status(500).json({ message: "Internel Server Error" });
-  }
-});
-
-//summary
-
-app.get("/api/transcations/summary/:user_Id", async (req, res) => {
-  try {
-    const { user_Id } = req.params;
-
-    const transactions =
-      await sql` Select COALESCE(SUM(amount),0)  as balance From transcations where user_id= ${user_Id} `;
-
-    const incomeResult =
-      await sql` Select COALESCE(SUM(amount),0)  as income From transcations where 
-      user_id= ${user_Id}  AND amount > 0`;
-
-    const expenseResult =
-      await sql` Select COALESCE(SUM(amount),0)  as expense From transcations where 
-      user_id= ${user_Id}  AND amount < 0`;
-
-    res.status(200).json({
-      balance:transactions[0].balance,
-      income:incomeResult[0].income,
-      expense:expenseResult[0].expense
-    });
-
-  } catch (error) {
-    console.log("Error in getting the transaction Summary", error);
-    res.status(500).json({ message: "Internel Server Error" });
-  }
-});
-
 console.log("my port", process.env.PORT);
+app.use("/api/transcations",transactionRoute)
 
 initDB().then(() => {
   app.listen(PORT, () => {
