8fbbc97c7abbf201f08d2b9a5d343f5bb09f709b
Akash Matlani <aakash.matlani@yahoo.com>
2025-10-22 14:14:42 +0100

    DB function code Updated

---
+++ backend/config/db.js
@@ -0,0 +1,27 @@
+import {neon} from "@neondatabase/serverless";
+import {sql} from "../config/db.js"
+
+import "dotenv/config";
+
+
+//Create a SQL connection using our DB URL
+
+export const sql= neon(process.env.DATABASE_URL); 
+
+export async function initDB() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transcations(
+        id SERIAL PRIMARY KEY,
+        user_id VARCHAR(255) NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        amount DECIMAL(10,2) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`;
+
+    console.log("Database initalized successfully");
+  } catch (error) {
+    console.log("error initalizing database", error);
+    process.exit(1); //status code 1 faliure , 0 Success
+  }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
-import transactionRoute from "./routes/transactionRoute.js"
+import transactionRoute from "./routes/transactionRoute.js";
+import { initDB } from "./config/db.js";
 
 dotenv.config();
 const app = express();
 
 //middleware
 
-app.use(rateLimiter)
+app.use(rateLimiter);
 
 app.use(express.json());
 
-
 const PORT = process.env.PORT || 5001;
-
-async function initDB() {
-  try {
-    await sql`CREATE TABLE IF NOT EXISTS transcations(
-        id SERIAL PRIMARY KEY,
-        user_id VARCHAR(255) NOT NULL,
-        title VARCHAR(255) NOT NULL,
-        amount DECIMAL(10,2) NOT NULL,
-        category VARCHAR(255) NOT NULL,
-        created_at DATE NOT NULL DEFAULT CURRENT_DATE
-        )`;
-
-    console.log("Database initalized successfully");
-  } catch (error) {
-    console.log("error initalizing database", error);
-    process.exit(1); //status code 1 faliure , 0 Success
-  }
-}
-
-console.log("my port", process.env.PORT);
-app.use("/api/transcations",transactionRoute)
+app.use("/api/transcations", transactionRoute);
 
 initDB().then(() => {
   app.listen(PORT, () => {
