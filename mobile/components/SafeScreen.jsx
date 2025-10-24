b3aa8e746a2fbcdada3ac4a23e78c01b3eea7690
Akash Matlani <aakash.matlani@yahoo.com>
2025-10-24 14:31:45 +0100

    SafeScreen style added

--- mobile/components/SafeScreen.jsx
+++ mobile/components/SafeScreen.jsx
@@ -1,14 +1,22 @@
+import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
+import {COLORS} from "@/constants/colors";
 
-import { View, Text } from 'react-native'
-import React from 'react'
+const SafeScreen = ({ children }) => {
+  const insets = useSafeAreaInsets();
 
-const SafeScreen = ({children}) => {
   return (
-    <View>
+    <View
+      style={{
+        paddingTop: insets.top,
+        flex: 1,
+        backgroundColor: COLORS.background,
+      }}
+    >
       {children}
     </View>
-  )
-}
+  );
+};
 
-export default SafeScreen
+export default SafeScreen;
