cca81523271291220258963ead1adf94e1caf760
Akash Matlani <aakash.matlani@yahoo.com>
2025-10-27 14:18:28 +0000

    Signup Import code updated

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,10 +1,14 @@
 import { Slot} from "expo-router";
 import { ClerkProvider } from '@clerk/clerk-expo'
+import SafeScreen from "../components/SafeScreen"
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
 
 export default function RootLayout() {
   return (
-    <ClerkProvider>
+    <ClerkProvider  tokenCache={tokenCache}>
+      <SafeScreen>
       <Slot />
+      </SafeScreen>
     </ClerkProvider>
   );
 }
