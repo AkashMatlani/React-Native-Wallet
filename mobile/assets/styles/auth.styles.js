41e420da3c6b4050f5686bd59f7b7213327ef742
Akash Matlani <aakash.matlani@yahoo.com>
2025-10-28 11:24:56 +0000

    Error Box design added

---
+++ mobile/assets/styles/auth.styles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+import { COLORS } from "../../constants/colors";
+
+export const styles = StyleSheet.create({
+  errorBox: {
+    backgroundColor: "#FFE5E5",
+    padding: 12,
+    borderRadius: 8,
+    borderLeftWidth: 4,
+    borderLeftColor: COLORS.expense,
+    marginBottom: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+  },
+
+  verificationContainer: {
+    flex: 1,
+    backgroundColor: COLORS.background,
+    padding: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
