f77b9e2be537f30285081c220908f699ecff9b99
Akash Matlani <aakash.matlani@yahoo.com>
2025-11-03 13:28:33 +0000

    Loader Added

--- mobile/app/(root)/index.jsx
+++ mobile/app/(root)/index.jsx
@@ -2,14 +2,39 @@ import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
 import { Link } from 'expo-router'
 import { Text, View } from 'react-native'
 import { SignOutButton } from '../../components/SignOutButton'
+import {useTransactions } from '../../hooks/useTransaction'
+import { useEffect } from 'react'
+import PageLoader, {pageLoader} from '../../components/PageLoader'
 
 export default function Page() {
   const { user } = useUser()
+
+  console.log("user:", user);
+ // const userId = user?.id; // avoid crash
+
+const userId = user?.id; 
+
+    const { transactions, summary, isLoading, loadData, deleteTransaction } = 
+    useTransactions(
+   userId
+  );
+
+
+  useEffect(()=>{
+    loadData();
+  },[loadData]);
+
+ // console.log("userid:------" ,user.id)
+  console.log("transaction:",transactions)
+  console.log("summary:",summary)
+
+if(isLoading) return <PageLoader/>
 
   return (
     <View>
       <SignedIn>
         <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+        <Text>{summary.income}</Text>
         <SignOutButton />
       </SignedIn>
       <SignedOut>
---
+++ mobile/assets/styles/home.styles.js
@@ -0,0 +1,13 @@
+
+import { StyleSheet } from "react-native";
+import { COLORS } from "../../constants/colors";
+
+export const homeStyle = StyleSheet.create({
+
+    loadingContainer:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor:COLORS.background
+    }
+});
