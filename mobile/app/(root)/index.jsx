8089d5873ebe2d3de0616599846f12cc21ed9667
Akash Matlani <aakash.matlani@yahoo.com>
2025-11-03 14:10:54 +0000

    Left Header UI and Style Set

--- mobile/app/(root)/index.jsx
+++ mobile/app/(root)/index.jsx
@@ -1,50 +1,56 @@
-import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
-import { Link } from 'expo-router'
-import { Text, View } from 'react-native'
-import { SignOutButton } from '../../components/SignOutButton'
-import {useTransactions } from '../../hooks/useTransaction'
-import { useEffect } from 'react'
-import PageLoader, {pageLoader} from '../../components/PageLoader'
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+import { SignOutButton } from "../../components/SignOutButton";
+import { useTransactions } from "../../hooks/useTransaction";
+import { useEffect } from "react";
+import PageLoader, { pageLoader } from "../../components/PageLoader";
+import { homeStyle } from "../../assets/styles/home.styles";
+import { Image } from "expo-image";
+import { styles } from "../../assets/styles/auth.styles";
 
 export default function Page() {
-  const { user } = useUser()
+  const { user } = useUser();
 
   console.log("user:", user);
- // const userId = user?.id; // avoid crash
+  // const userId = user?.id; // avoid crash
 
-const userId = user?.id; 
+  const userId = user?.id;
 
-    const { transactions, summary, isLoading, loadData, deleteTransaction } = 
-    useTransactions(
-   userId
-  );
+  const { transactions, summary, isLoading, loadData, deleteTransaction } =
+    useTransactions(userId);
 
-
-  useEffect(()=>{
+  useEffect(() => {
     loadData();
-  },[loadData]);
+  }, [loadData]);
 
- // console.log("userid:------" ,user.id)
-  console.log("transaction:",transactions)
-  console.log("summary:",summary)
+  // console.log("userid:------" ,user.id)
+  console.log("transaction:", transactions);
+  console.log("summary:", summary);
 
-if(isLoading) return <PageLoader/>
+  if (isLoading) return <PageLoader />;
 
   return (
-    <View>
-      <SignedIn>
-        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-        <Text>{summary.income}</Text>
-        <SignOutButton />
-      </SignedIn>
-      <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text>Sign in</Text>
-        </Link>
-        <Link href="/(auth)/sign-up">
-          <Text>Sign up</Text>
-        </Link>
-      </SignedOut>
+    <View style={homeStyle.container}>
+      <View style={homeStyle.content}>
+        {/* Headrer */}
+        <View style={homeStyle.header}>
+          {/* Left */}
+            <View style={homeStyle.headerLeft}>
+            <Image source={require("../../assets/images/logo.png")}
+            style={homeStyle.headerLogo}
+            contentFit="contain">
+            </Image>
+
+            <View style={homeStyle.welcomeContainer}>
+              <Text style={homeStyle.welcomeText}>Welcome,</Text>
+                <Text style={homeStyle.userNameText}>{user.emailAddresses[0]?.emailAddress.split("@")[0]}
+                </Text>
+              </View>
+            </View>
+            
+            </View>
+        </View>
     </View>
-  )
+  );
 }
--- mobile/assets/styles/home.styles.js
+++ mobile/assets/styles/home.styles.js
@@ -1,13 +1,49 @@
-
 import { StyleSheet } from "react-native";
 import { COLORS } from "../../constants/colors";
 
 export const homeStyle = StyleSheet.create({
-
-    loadingContainer:{
-        flex:1,
-        justifyContent:"center",
-        alignItems:"center",
-        backgroundColor:COLORS.background
-    }
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: COLORS.background,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.background,
+  },
+  content: {
+    padding: 20,
+    paddingBottom: 0,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+    paddingHorizontal: 0,
+    paddingVertical: 12,
+  },
+  headerLeft: {
+    alignItems: "center",
+    flexDirection: "row",
+    flex: 1,
+  },
+  headerLogo: {
+    height: 75,
+    width: 75,
+  },
+  welcomeContainer: {
+    flex: 1,
+  },
+  welcomeText: {
+    fontSize: 14,
+    color: COLORS.textLight,
+    marginBottom: 2,
+  },
+  userNameText: {
+    fontSize: 16,
+    fontWeight: "600",
+    color: COLORS.text,
+  },
 });
