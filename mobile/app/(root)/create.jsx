c102621e068e9c00d4a449d5cb76d5217f1c4d3f
Akash Matlani <aakash.matlani@yahoo.com>
2025-11-11 12:27:12 +0000

    Create Transaction API call added

--- mobile/app/(root)/create.jsx
+++ mobile/app/(root)/create.jsx
@@ -1,13 +1,14 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React, { useState } from "react";
 import { useRouter } from "expo-router";
 import { useUser } from "@clerk/clerk-expo";
 import { styles } from "../../assets/styles/create.styles";
 import { Ionicons } from "@expo/vector-icons";
 import { COLORS } from "../../constants/colors";
+import { API_URL } from "../constants/api";
 
 const CATEGORIES = [
-  { id: "food", name: "Food & Drinks", icon: "fast_food" },
+  { id: "food", name: "Food & Drinks", icon: "fast-food" },
   { id: "shopping", name: "Shopping", icon: "cart" },
   { id: "transporation", name: "Transporation", icon: "car" },
   { id: "entertainment", name: "Entertainment", icon: "film" },
@@ -15,11 +16,63 @@ const CATEGORIES = [
   { id: "income", name: "Income", icon: "cash" },
   { id: "other", name: "Other", icon: "ellipsis-horizontal" },
 ];
- const CreateScreen = () => {
+const CreateScreen = () => {
   const router = useRouter();
   const { user } = useUser();
 
   const [selectdCategory, setSelectedCategory] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [isExpense, setIsExpense] = useState(false);
+
+  const handleCreate = async () => {
+    //validations
+    if (!title.trim())
+      return Alert.alert("Error", "Please enter a transaction title");
+    if (!amount || isNaN(parseFloat(amount)) || parseFloat(amount) <= 0) {
+      Alert.alert("Error", "Please enter a valid amount");
+      return;
+    }
+
+    if (!selectdCategory)
+      return Alert.alert("Error", "Please select a category");
+
+    setIsLoading(true);
+    try {
+      //formatting amount (negative for expsenses, positive for income)
+      const formattedAmount = isExpense
+        ? -Math.abs(parseFloat(amount))
+        : Math.abs(parseFloat(amount));
+
+      const response = await fetch(`${API_URL}/transcations`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify({
+          userId: user.id,
+          title,
+          amount: formattedAmount,
+          category: selectdCategory,
+        }),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error || "Failed to create transaction");
+      }
+
+      Alert.alert("Success", "Transaction created Successfully");
+      router.back();
+    } catch (error) {
+      Alert.alert("Error", error.message, "Failed to create transaction");
+      consle.error("Error Creating transaction:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <View style={styles.categoryGrid}>
@@ -33,7 +86,7 @@ const CATEGORIES = [
           onPress={() => setSelectedCategory(category.name)}
         >
           <Ionicons
-            name={category.name}
+            name={category.icon}
             size={20}
             color={
               selectdCategory === category.name ? COLORS.white : COLORS.text
